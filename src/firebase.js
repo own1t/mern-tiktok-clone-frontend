f80cae633eddd4b89f6c49ac2c0f78703e13df4e
Ryan Kim <iawal86@gmail.com>
2020-09-20 18:47:18 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOCKxJrPuqziNuaNoUHnlq1iuDPGbc8fk",
+  authDomain: "tiktok-clone-8224a.firebaseapp.com",
+  databaseURL: "https://tiktok-clone-8224a.firebaseio.com",
+  projectId: "tiktok-clone-8224a",
+  storageBucket: "tiktok-clone-8224a.appspot.com",
+  messagingSenderId: "530502992268",
+  appId: "1:530502992268:web:24baaaca7ea4185da796a9",
+};
